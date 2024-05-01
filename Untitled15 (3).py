06ee7a7ccc11f9f760c533772b4db4ab38197263
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-05-01 15:05:53 +0530

    Add files via upload

---
+++ Untitled15 (3).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[47]:
+
+
+l=[]
+n=int(input())
+for i in range (n):
+    n1=int(input())
+    l.append(n1)
+
+
+s=0
+for x in l:
+    if x%2==0:
+        s=s+x
+print(s)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
